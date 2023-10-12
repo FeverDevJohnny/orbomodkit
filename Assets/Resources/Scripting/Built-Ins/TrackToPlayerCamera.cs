c2d772251d4b74d31336ca0564490a9020bb28cb
Feverdream Johnny <johnwepersonal@gmail.com>
2023-10-11 20:18:33 -0400

    - Updated track to player head to allow for transform alignment - Added new component to track to the player component - A lot of corrections in the main game code

---
+++ Assets/Resources/Scripting/Built-Ins/TrackToPlayerCamera.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackToPlayerCamera : MonoBehaviour
+{
+    public bool alignToTransform = false;
+}
--- Assets/Resources/Scripting/Built-Ins/TrackToPlayerHead.cs
+++ Assets/Resources/Scripting/Built-Ins/TrackToPlayerHead.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class TrackToPlayerHead : MonoBehaviour
 {
     public bool useFacecamInstead;
+    public bool alignToTransform;
 }
